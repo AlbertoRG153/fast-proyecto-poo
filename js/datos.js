8c0975d3add83cad1d73802b9365e37722a53ff5
cruz-IS <ercruzb@unah.hn>
2022-07-17 23:55:33 -0600

    Avance del Js

---
+++ js/datos.js
@@ -0,0 +1,273 @@
+
+var usuarios = [
+    {
+        nombre:"Luis",
+        apellido:"Rodriguez",
+        ordenes:[
+            {
+                nombreProducto:"Pizza de Peperoni",
+                descripcion: "",
+                cantidad:2,
+                precio:0
+            },
+
+            {
+                nombreProducto:"Pollo Frito",
+                descripcion: "",
+                cantidad:2,
+                precio:0
+            }
+        ]
+    },
+    {
+        nombre:"Mainor",
+        apellido:"Zuniga",
+        ordenes:[
+            {
+                nombreProducto:"Carne Guisada",
+                descripcion: "",
+                cantidad:2,
+                precio:0
+            },
+
+            {
+                nombreProducto:"Pollo Empanizado",
+                descripcion: "",
+                cantidad:2,
+                precio:0
+            }
+        ]
+    },
+    {
+        nombre:"Gisella",
+        apellido:"Martinez",
+        ordenes:[
+            {
+                nombreProducto:"Pollo Asado",
+                descripcion: "",
+                cantidad:2,
+                precio:0
+            },
+
+            {
+                nombreProducto:"Pizza de Peperoni",
+                descripcion: "",
+                cantidad:2,
+                precio:0
+            }
+        ]
+    }
+];
+
+
+var categorias = [
+    {
+        nombreCategoria:"Pizzas",
+        descripcion:"",
+        empresas:[
+            {
+                nombreEmpresa: "pizza Hut",
+                imagen:"",
+                productos:[
+                    {
+                        nombreProducto: "Pizza de Peperoni",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Pizza con doble queso",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Alistas de pollo",
+                        descripcion: "",
+                        precio: 0
+                    }
+                ]
+            },
+            {
+                nombreEmpresa: "Little Caesars",
+                imagen:"",
+                productos:[
+                    {
+                        nombreProducto: "Pizza de jamon",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Pizza con vegetales",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Pan de ajo",
+                        descripcion: "",
+                        precio: 0
+                    }
+                ]
+            },
+            {
+                nombreEmpresa: "Dominos",
+                imagen:"",
+                productos:[
+                    {
+                        nombreProducto: "Pizza premium",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "ensalada de Vegetales",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Pizza Suprema",
+                        descripcion: "",
+                        precio: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        nombreCategoria:"Hamburguesas",
+        descripcion:"",
+        empresas:[
+            {
+                nombreEmpresa: "Burger king",
+                imagen:"",
+                productos:[
+                    {
+                        nombreProducto: "PHamburguesa Bacon",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "hamburguesa king",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Submarino Supremo",
+                        descripcion: "",
+                        precio: 0
+                    }
+                ]
+            },
+            {
+                nombreEmpresa: "Bigos",
+                imagen:"",
+                productos:[
+                    {
+                        nombreProducto: "Hamburguesa Suprema ",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Pepito",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Hamburguesa con doble carne",
+                        descripcion: "",
+                        precio: 0
+                    }
+                ]
+            },
+            {
+                nombreEmpresa: "Wendy's",
+                imagen:"",
+                productos:[
+                    {
+                        nombreProducto: "PHamburguesa King",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Pan encebollado",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Hamburguesa mixta",
+                        descripcion: "",
+                        precio: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        nombreCategoria:"Restaurantes Caceros",
+        descripcion:"",
+        empresas:[
+            {
+                nombreEmpresa: "El patio",
+                imagen:"",
+                productos:[
+                    {
+                        nombreProducto: "Carne Guisada",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Chuleta de cerdo",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Pollo Asado",
+                        descripcion: "",
+                        precio: 0
+                    }
+                ]
+            },
+            {
+                nombreEmpresa: "Kentucky",
+                imagen:"",
+                productos:[
+                    {
+                        nombreProducto: "Pollo Picante",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "pollo Empanisado",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Deditos de Pollo",
+                        descripcion: "",
+                        precio: 0
+                    }
+                ]
+            },
+            {
+                nombreEmpresa: "Popeyes",
+                imagen:"",
+                productos:[
+                    {
+                        nombreProducto: "Pechuga de Pollo",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Ensalada de Vegetales",
+                        descripcion: "",
+                        precio: 0
+                    },
+                    {
+                        nombreProducto: "Arroz con frijoles",
+                        descripcion: "",
+                        precio: 0
+                    }
+                ]
+            }
+        ]
+    },
+];
+
+
