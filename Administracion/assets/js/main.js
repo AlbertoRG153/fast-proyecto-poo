32b07de0a4d06c3744044251db0652455108343a
Alberto Romero <alberto.romerog27@gmail.com>
2022-08-04 23:28:01 -0600

    Avance en Perfil y Ordenes

---
+++ Administracion/assets/js/main.js
@@ -0,0 +1,20 @@
+
+var irPerfil = () => {
+    location.href = "perfil.html";
+}
+
+var irOrdenes = () => {
+    location.href = "index.html";
+}
+
+var irMotoristas = () => {
+    location.href = "motoristas.html";
+}
+
+var irProductos = () => {
+    location.href = "productos.html";
+}
+
+var irEmpresas = () => {
+    location.href = "empresas.html";
+}
