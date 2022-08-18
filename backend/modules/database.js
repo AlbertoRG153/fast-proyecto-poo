2951010dee0e85ec049168efb1c187850971ffbf
Alberto Romero <alberto.romerog27@gmail.com>
2022-08-18 01:49:00 -0600

    Detalle

--- backend/modules/database.js
+++ backend/modules/database.js
@@ -7,22 +7,22 @@ let port = '27017';
 //hosting o donde esta ubicado la base de datos en que equipo o servidor
 let host = 'localhost';
 
-class Database{
-    constructor(){
+class Database {
+    constructor() {
         this.conectar();
     }
 
-    conectar(){
+    conectar() {
         //mongoose.connect(`mongodb://${host}:${port}/${bd}`)
         mongoose.connect(
-            process.env.MONGODB_URI
+            'mongodb+srv://edwar:edwar2022@fast-proyecto.p4xaw7g.mongodb.net/?retryWrites=true&w=majority'
         )
-        .then(()=>{
-            console.log('Se conecto a mongodb Atlas');
-        })
-        .catch(error=>{
-            console.error(JSON.stringify(error));
-        });
+            .then(() => {
+                console.log('Se conecto a mongodb Atlas');
+            })
+            .catch(error => {
+                console.error(JSON.stringify(error));
+            });
     }
 }
 
